f49415a7e4b8e444607576762666db8e5604f495
Mark Soderquist <mark@soderquist.net>
2020-12-20 13:48:40 -0700

    Rename master branch to main

--- config.sh
+++ config.sh
@@ -9,9 +9,9 @@ chmod 600 "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa.pub"
 chmod 600 "${HOME}/.ssh/known_hosts"
 
-#AVN_RELEASE is derived from github.ref, example values: refs/heads/master, refs/heads/stable
+#AVN_RELEASE is derived from github.ref, example values: refs/heads/main, refs/heads/stable
 case "${GITHUB_REF}" in
-  "refs/heads/master") AVN_RELEASE="latest" ;;
+  "refs/heads/main") AVN_RELEASE="latest" ;;
   "refs/heads/stable") AVN_RELEASE="stable" ;;
 esac
 export PRODUCT_DEPLOY_PATH=/opt/avn/store/${AVN_RELEASE}/${AVN_PRODUCT}
